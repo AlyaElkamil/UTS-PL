18241ba32acc2132da72747f4fe322cd1cacc1e6
AlyaElkamil <151538175+AlyaElkamil@users.noreply.github.com>
2024-04-23 10:34:02 +0700

    Add files via upload

---
+++ DataX.java
@@ -0,0 +1,16 @@
+package OOPClass;
+
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
+public class DataX {
+    public static void main (String[] args) {
+        String Filename = ("https://gist.github.com/affandes/0eac4b72c299a8024d90d57ec91859c1.js">);
+        MainX data = new MainX(39.76,52.18);
+        this.panjang * this.lebar = this.luas;
+
+        ArrayList<File> Filename = new ArrayList();
+    }
+}
---
+++ MainX.java
@@ -0,0 +1,45 @@
+package OOPClass;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainX {
+    String Filename;
+    double panjang;
+    double lebar;
+    double luas;
+    int a;
+    int b;
+    int c;
+
+    MainX(double panjang, double lebar) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+        panjang = a;
+        lebar = b;
+    }
+
+    void getFilename() {
+        FileReader File = new FileReader();
+        String File = //gist.github.com/affandes/0eac4b72c299a8024d90d57ec91859c1;
+                ArrayList<String> Filename = new ArrayList<>();
+        Filename.add(Edge A);
+        Filename.add(Edge B);
+        Filename.add(Edge C);
+        }
+
+        void runFilename () {
+            getFilename("https://gist.github.com/affandes/0eac4b72c299a8024d90d57ec91859c1.js">);
+        }
+
+    void getLuas() {
+        return this.panjang * this.lebar;
+        luas = c;
+        }
+
+        public void print() {
+    System.out.println("Panjang: " +panjang+ ", lebar: " +lebar+ ".");
+            System.out.println("Luas: " +luas);
+        }
+}
